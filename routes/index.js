cd26ebd226883840bb140ffa15f43abf76520c2c
jason00111 <emberleyjason@gmail.com>
2017-07-25 11:40:11 -0700

    refactors routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const database = require('../database')
 
+const signupRoute = require('./signup')
+const signinRoute = require('./signin')
+const albumsRoute = require('./albums')
+const reviewsRoute = require('./reviews')
+const usersRoute = require('./users')
+
 router.get('/', (request, response) => {
   database.getAlbums((error, albums) => {
     if (error) {
@@ -15,6 +21,21 @@ router.get('/', (request, response) => {
       })
     }
   })
+})
+
+router.get('/signout', (request, response) => {
+  request.session = null
+  response.redirect('/')
+})
+
+router.use('/signup', signupRoute)
+router.use('/signin', signinRoute)
+router.use('/albums', albumsRoute)
+router.use('/reviews', reviewsRoute)
+router.use('/users', usersRoute)
+
+router.use((request, response) => {
+  response.status(404).render('not_found', { userID: request.session.userID })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
-const database = require('./database')
 const app = express()
 
 const keys = require('./keys')
-const signupRoute = require('./routes/signup')
-const signinRoute = require('./routes/signin')
-const albumsRoute = require('./routes/albums')
-const reviewsRoute = require('./routes/reviews')
-const usersRoute = require('./routes/users')
-const indexRoute = require('./routes')
+const routes = require('./routes')
 
 require('ejs')
 app.set('view engine', 'ejs');
@@ -23,26 +17,7 @@ app.use(cookieSession({
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', indexRoute)
-
-app.use('/signup', signupRoute)
-
-app.use('/signin', signinRoute)
-
-app.use('/albums', albumsRoute)
-
-app.use('/reviews', reviewsRoute)
-
-app.use('/users', usersRoute)
-
-app.get('/signout', (request, response) => {
-  request.session = null
-  response.redirect('/')
-})
-
-app.use((request, response) => {
-  response.status(404).render('not_found', { userID: request.session.userID })
-})
+app.use('/', routes)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
