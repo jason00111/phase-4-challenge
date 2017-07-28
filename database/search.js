65a70e6b2435b65704e5d8f1006281aa1000b8c8
jason00111 <emberleyjason@gmail.com>
2017-07-28 12:45:37 -0700

    adds artists to search

--- database/search.js
+++ database/search.js
@@ -1,7 +1,8 @@
 const query = require('./setup')
 
 const search = queryString =>
-  query("SELECT * FROM albums WHERE title ILIKE $1", [`%${queryString}%`])
+  query("SELECT * FROM albums WHERE title ILIKE $1 OR artist ILIKE $1",
+    [`%${queryString}%`])
 
 module.exports = {
   search
